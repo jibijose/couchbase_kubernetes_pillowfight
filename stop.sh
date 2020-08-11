8d349d27828c14f2845724ebefa57577af9dca95
Jibi Jose <jibijose@yahoo,com>
2020-08-11 22:09:59 +0530

    helm integrated

--- stop.sh
+++ stop.sh
@@ -3,6 +3,7 @@
 NAMESPACE_NAME=pillowfight
 
 echo `date`"   ### Deleting kubernetes yamls to namespace ${NAMESPACE_NAME}"
-kubectl delete -f k8s.yaml -n ${NAMESPACE_NAME}
+#kubectl delete -f k8s.yaml -n ${NAMESPACE_NAME}
+helm uninstall pillowfight -n ${NAMESPACE_NAME}
 kubectl delete namespace ${NAMESPACE_NAME}
 echo `date`"   ### All cleaned up"
